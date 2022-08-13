a068670cc1ed954a5422514173179f5967516387
sherxonDeveloper <sherxontoshpulatov3@gmail.com>
2022-08-13 19:20:12 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+   let names = prompt ("Ismingizni kiriting!")
+   let lastname = prompt ("Familiyangizni kiriting!")
+   let age = prompt ("Yoshingizni kiriting!")
+   let region = prompt ("Tug'ilgan joyingizni kiriting!")
+   let numbers = prompt ("Telefon nomiringizni kiriting!")
+   let dtudy = prompt ("Qaterda uqiysiz")
+   let nutions = prompt ("Yunalishezni kiriting")
+
+
+   alert(`
+   Ism - ${names}
+   Familiya - ${lastname}
+   Yosh - ${age}
+   To'g'ilgan joy - ${region}
+   Tell - ${numbers}
+   Malumot - ${nutions}
+ `);
+
+   console.log(`
+   Ism - ${names}
+   Familiya - ${lastname}
+   Yosh - ${age}
+   To'g'ilgan joy - ${region}
+   Tell - ${numbers}
+   Malumot - ${nutions}
+   `); 
+
+
+
+
+
